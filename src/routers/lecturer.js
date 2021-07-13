b677ad6f6c009a9bd3d0f3cfb8b17475b8cf470b
abdullah18x <abdullah18x@gmail.com>
2021-07-13 23:50:27 +0500

    updated lecturer bulk upload

--- src/routers/lecturer.js
+++ src/routers/lecturer.js
@@ -3,6 +3,7 @@ const sql = require('../config/mysqlConfig')
 const token = require('../methods/generateToken')
 const auth = require('../middleware/lecturerAuth')
 const auth2 = require('../middleware/adminAuth')
+const csv=require('csvtojson')
 const multer = require('multer')
 var path = require('path')
 const fs = require("fs");
@@ -316,40 +317,29 @@ router.post('/getLectrerStatus', auth, async (req,res) => {
     }
 })
 
-router.post('/addBulkTeachers',auth, upload.single('csvFile'), async (req,res) => {
-    let title = req.body.title
-    let totalMarks = req.body.totalMarks
-    let timeNeeded = req.body.timeNeeded
-    let ifC = req.body.ifC
-    let switchC = req.body.switchC
-    let whileL = req.body.whileL
-    let dowhileL = req.body.dowhileL
-    let forL = req.body.forL
-    let multipleClasses = req.body.multipleClasses
-    let methods = req.body.methods
-    let arrays = req.body.arrays
-    let expectedAnsType = req.body.expectedAnsType
-    let subjectId = req.body.subjectId
-    let resourceMaterial = req.file.filename
-    let details = req.body.details
-    let resourceLinks = req.body.resourceLinks
-    let expectedAns = req.body.expectedAns
-    let solution = req.body.solution
-
-    let query = 'INSERT INTO datasets(title, totalMarks, timeNeeded, ifC, switchC, whileL, dowhileL, forL, multipleClasses, methods, arrays, expectedAnsType, subjectId, resourceMaterial, details, resourceLinks,expectedAns, solution) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
-    try {
-        // if (!file) {
-        //     const error = new Error('Please upload a file')
-        //     error.httpStatusCode = 400
-        //     console.log(error) 
-        //   }
-        let conn = await sql.getDBConnection();
-        await conn.execute(query,[title,totalMarks,timeNeeded,ifC,switchC,whileL,dowhileL,forL,multipleClasses,methods,arrays,expectedAnsType,subjectId,resourceMaterial,details,resourceLinks,expectedAns,solution])
-        res.status(200).send('Inserted assignment')
-    } catch (error) {
-        res.status(400).send(error)
-        console.log(error)
-    }
+router.post('/addBulkLecturers',auth, upload.single('csvFile'), async (req,res) => {
+    console.log(req.file)
+    const csvFilePath=req.file.path
+    csv()
+      .fromFile(csvFilePath)
+      .then((jsonObj) => {
+          
+        console.log(jsonObj);
+      });
+    // let query = 'INSERT INTO datasets(title, totalMarks, timeNeeded, ifC, switchC, whileL, dowhileL, forL, multipleClasses, methods, arrays, expectedAnsType, subjectId, resourceMaterial, details, resourceLinks,expectedAns, solution) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
+    // try {
+    //     // if (!file) {
+    //     //     const error = new Error('Please upload a file')
+    //     //     error.httpStatusCode = 400
+    //     //     console.log(error) 
+    //     //   }
+    //     let conn = await sql.getDBConnection();
+    //     await conn.execute(query,[title,totalMarks,timeNeeded,ifC,switchC,whileL,dowhileL,forL,multipleClasses,methods,arrays,expectedAnsType,subjectId,resourceMaterial,details,resourceLinks,expectedAns,solution])
+    //     res.status(200).send('Inserted assignment')
+    // } catch (error) {
+    //     res.status(400).send(error)
+    //     console.log(error)
+    // }
 })
 
 //Delete a Lecturer
